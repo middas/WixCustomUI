7b29b5101e4d65f1fe241735990c7361c7046635
middas <justin.middas@gmail.com>
2020-05-12 01:57:57 -0600

    Added Bootstrapper.UI project

---
+++ Bootstrapper.UI/BootstrapperEntry.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Tools.WindowsInstallerXml.Bootstrapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bootstrapper.UI
+{
+    public class BootstrapperEntry : BootstrapperApplication
+    {
+        protected override void Run()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
